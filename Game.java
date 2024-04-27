22262075b629f00bd9e9fae88a4790551be5caac
ayano <atamura@smith.edu>
2024-04-27 17:56:26 -0400

    part 2 updated intro

--- Game.java
+++ Game.java
@@ -17,24 +17,30 @@ public class Game{
         Scanner scanner = new Scanner(System.in);
         boolean coinLocker = false;
         boolean phoneMessage = false;
+        boolean closeMessage = false;
 
         while (true){
             System.out.println("What would you like to do?");
             String userInput = scanner.nextLine();
 
-            if (userInput.equals("open coin locker")){
+            if (userInput.toLowerCase().equals("open coin locker")){
                 System.out.println("Opening the coin locker reveals a phone that has a message.");
                 coinLocker = true;
-            } else if (coinLocker && userInput.equals("Read message")){
+            } else if (coinLocker && userInput.toLowerCase().equals("read message")){
                 System.out.println("-Message Opened-");
                 System.out.println("Welcome to Japan! In this interavtive adventure game, you will explore different prefectures in the country, and complete missions along the way. There is a train waiting for you to board. Good luck!");
                 phoneMessage = true;
             } else if (!coinLocker){
-                System.out.println("You must open the coin locker to play the game");
+                System.out.println("Sorry, I didn't quite get that. You must open the coin locker to play the game");
             } else if (coinLocker && !phoneMessage){
                 System.out.println("Sorry, I didn't get that. There is a message on the phone");
+            } else if (coinLocker && phoneMessage && userInput.toLowerCase().equals("close message")){
+                System.out.println("-Message Closed-");
+                closeMessage = true;
+            } else if (coinLocker && phoneMessage && !closeMessage){
+                System.out.println("The message is still opened, you have to close it first!");
             }
-            if (phoneMessage){
+            if (closeMessage){
                 break;
             }
         }
