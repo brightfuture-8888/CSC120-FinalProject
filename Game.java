bede7949017dad5bd9593892c61fbc1f748b5ac6
ayano <atamura@smith.edu>
2024-04-29 17:02:24 -0400

    game intro completed and site structures

--- Game.java
+++ Game.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.HashSet;
 // import com.google.common.graph.*;
 
 // public class Game<Site> {
@@ -8,16 +9,24 @@ import java.util.Scanner;
     
 // }
 public class Game{
+    private Scanner scanner;
+    private HashSet <String> userLocations;
+
+    public Game(){
+        userLocations = new HashSet<>();
+    }
+
     public void start(){
         System.out.println("--------------------------------------------------------------------------");
         System.out.println("You are standing in the middle of Tokyo station. There is a coin locker here.");
         play();
     }
     public void play(){
-        Scanner scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
         boolean coinLocker = false;
         boolean phoneMessage = false;
         boolean closeMessage = false;
+        boolean boardTrain = false;
 
         while (true){
             System.out.println("What would you like to do?");
@@ -28,6 +37,7 @@ public class Game{
                 coinLocker = true;
             } else if (coinLocker && userInput.toLowerCase().equals("read message")){
                 System.out.println("-Message Opened-");
+                System.out.println("");
                 System.out.println("Welcome to Japan! In this interavtive adventure game, you will explore different prefectures in the country, and complete missions along the way. There is a train waiting for you to board. Good luck!");
                 phoneMessage = true;
             } else if (!coinLocker){
@@ -39,16 +49,53 @@ public class Game{
                 closeMessage = true;
             } else if (coinLocker && phoneMessage && !closeMessage){
                 System.out.println("The message is still opened, you have to close it first!");
+            } else if (coinLocker && phoneMessage && closeMessage && userInput.toLowerCase().equals("board train")){
+                System.out.println("Boarding Train...");
+                scanner.nextLine();
+                System.out.println("-Train Boarded-");
+                boardTrain = true;
+            } else if (coinLocker && phoneMessage && closeMessage && !boardTrain){
+                System.out.println("Directions are not clear. Board the train to resume journey.");
             }
-            if (closeMessage){
+            if (boardTrain){
                 break;
             }
         }
-        scanner.close();
+        train();
     }
+
+    public void train(){
+
+        System.out.println("You have now boarded the train! Choose a location to go to:");
+        while (true){
+            String userInput = scanner.nextLine();
+            if(userLocations.contains(userInput)){
+                System.out.println("You have already visited this location. Please pick another location");
+            } else if (userInput.toLowerCase().equals("yamanashi")){
+                System.out.println("Headed to Yamanashi...");
+                scanner.nextLine();
+                Site site = new Site();
+                site.Yamanashi();
+                userLocations.add(userInput);
+                break;
+            // } else if (userInput.toLowerCase().equals("Nara")){
+            //     Site site = new Site();
+            //     site.Nara();
+            //     userLocations.add(userInput);
+            //     break;
+            } else {
+                System.out.println("Sorry, that location does not exist. Where would you like to go?");
+            }
+        }
+        // scanner = new Scanner(System.in);
+
+    }
+
+    // }
     public static void main(String[] args) {
         Game game = new Game();
         game.start();
+        game.scanner.close();
     }
     
 }
--- Site.java
+++ Site.java
@@ -1,99 +1,63 @@
 import java.util.Scanner;
 
 public class Site {
-    // String name;
-    // String question;
+
     String response;
     boolean checkAnswer;
 
-    int answerYNorth;
-    int answerYSouth;
+    int answerNorth;
+    int answerSouth;
 
-    public void Yamanashi(int answerYNorth, int answerYSouth) {
-        // this.name = name;
-        // this.question = question;
-        this.answerYNorth = answerYNorth;
-        this.answerYSouth = answerYSouth;
+    public void Yamanashi() {
 
-        Scanner direction = new Scanner(System.in);
-        System.out.println("North or South?");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("You have arrived at Yamanashi!");
+
+        System.out.println("Would you like to go North or South?");
     
-        String response = direction.nextLine();
-        System.out.println("You are going to" + " " + response);
-        
-        direction.close();
+        String response = scanner.nextLine();
+        System.out.println("You are going " + response);
 
-        if (response == "north") {
-
-            Scanner input = new Scanner(System.in);
-            this.answerYNorth = input.nextInt();
-            input.close();
+        if (response.toLowerCase().equals("north"));{
 
             System.out.println("You're at Kouhu-shi");
+            System.out.println("");
             System.out.println("How old is the oldest person in Yamanashi prefecture?");
+            int answerNorth = scanner.nextInt();
 
-            if (answerYNorth == 112) {
+            if (answerNorth == 112) {
                 System.out.println("Congrats!! You are right!! You gained a token");
             } else {
                 System.out.println("You are incorrect! You lost a token.");
             }
+            scanner.close();
         }
 
         if (response == "south") {
 
-            Scanner input = new Scanner(System.in);
-            this.answerYSouth = input.nextInt();
-            input.close(); 
-
-            System.out.println("You're at Mt.Fuji");
+            System.out.println("You are at Mt.Fuji");
+            System.out.println("");
             System.out.println("Guess the height of Mt. Fuji in meters (no commas)");
 
-            if (answerYSouth == 3776) {
+            int answerSouth = scanner.nextInt();
+            if (answerSouth == 3776) {
                 System.out.println("Congrats!! You are right!! You gained a token");
             } else {
                 System.out.println("You are incorrect! You lost a token.");
             }
+            System.out.println("");
+            scanner.close();
         }
     }
 
-    // public void Nara(String name, String question, int answer) {
-    // this.name = name;
-    // this.question = question;
-    // this.answer = answer;
-    // }
+    // public void Nara {}
 
-    // public void Osaka(String name, String question, int answer) {
-    // this.name = name;
-    // this.question = question;
-    // this.answer = answer;
-    // }
+    // public void Osaka() {}
 
-    // public void Niigata(String name, String question, int answer) {
-    // this.name = name;
-    // this.question = question;
-    // this.answer = answer;
-    // }
+    // public void Niigata() {}
 
-    // public void Kyoto(String name, String question, int answer) {
-    // this.name = name;
-    // this.question = question;
-    // this.answer = answer;
-    // }
+    // public void Kyoto() {}
 
-    // public void Saitama(String name, String question, int answer) {
-    // this.name = name;
-    // this.question = question;
-    // this.answer = answer;
-    // }
+    // public void Saitama() {}
 
-    // public void tokyoStation(String name, String question, int answer) {
-    // this.name = name;
-    // this.question = question;
-    // this.answer = answer;
-    // }
-
-    public static void main(String[] args) {
-         Site mySite = new Site();
-         mySite.Yamanashi(112,3776); 
-    }
 }
